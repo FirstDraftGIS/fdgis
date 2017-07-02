52cd853c9722cb027e21272dc5e8815747d8432c
danieljdufour <daniel.j.dufour@gmail.com>
2017-07-02 17:52:31 +0000

    Added validators to setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
   packages = ['fdgis'],
   package_dir = {'fdgis': 'fdgis'},
   package_data = {'fdgis': ['__init__.py', 'timeout/__init__.py']},
-  version = '2.5',
+  version = '2.6',
   description = 'Makes the first draft of your map',
   author = 'Daniel J. Dufour',
   author_email = 'daniel.j.dufour@gmail.com',
@@ -13,5 +13,5 @@ setup(
   download_url = 'https://github.com/FirstDraftGIS/fdgis/tarball/download',
   keywords = ['geocoding','geojson','gis','maps','nlp','python'],
   classifiers = [],
-  install_requires=["Pillow", "requests"]
+  install_requires=["Pillow", "requests", "validators"]
 )
